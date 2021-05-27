e1168921387b2814a68938b45bb7d066b4a0cfa8
Amir Khan <ak9471714@gmail.com>
2021-05-27 18:29:05 +0530

    Create schema for post, save the post in the db, show post and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,15 @@
 const User = require('../models/user');
+const Post = require('../models/post');
 
 module.exports.home = function(req, res){
-    res.render('home', {
-        title: 'friendlink',
-    });
+    
+    // populate the user of each post
+    Post.find({}).populate('user').exec(
+        function(err, posts){
+            res.render('home', {
+                title: 'friendlink',
+                posts: posts
+            });
+        }
+    );
 }
