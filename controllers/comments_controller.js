4200d8461d400e5106f9221a1118426402c6d2d1
Amir Khan <ak9471714@gmail.com>
2021-05-27 22:02:33 +0530

    Add comments into the Database

---
+++ controllers/comments_controller.js
@@ -0,0 +1,25 @@
+const Comment = require('../models/comment');
+const Post = require('../models/post');
+
+module.exports.create = function(req, res){
+    Post.findById(req.body.post, function(err, post){
+        if(post){
+            Comment.create({
+                content: req.body.content,
+                post: req.body.post,
+                user : req.user._id
+            }, function(err, comment){
+                if(err){
+                    console.log('Error in creating comment', err);
+                    return;
+                }
+
+                // if comment is created add this
+                // comment to the post's comments array
+                post.comments.push(comment);
+                // save after upadte
+                post.save();
+            });
+        }
+    });
+};
--- models/comment.js
+++ models/comment.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema({
     content: {
-        typE: String,
+        type: String,
         required: true
     },
     user: {
