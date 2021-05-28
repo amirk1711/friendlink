0093637eeb864d866736a2095382a745b6aa05db
Amir Khan <ak9471714@gmail.com>
2021-05-28 08:02:51 +0530

    Delete posts

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/post');
 
 module.exports.create = function(req, res){
     Post.create({
@@ -10,5 +11,26 @@ module.exports.create = function(req, res){
             return;
         }
         return res.redirect('back');
+    });
+}
+
+
+module.exports.destroy = function(req, res){
+    Post.findById(req.params.id, function(err, post){
+        // .id means converting the object id into string
+        if(post.user == req.user.id){
+            post.remove();
+
+            Comment.deleteMany({post: req.params.id}, function(err){
+                if(err){
+                    console.log('Error in deleting Comments associated with the post');
+                    return;
+                }
+                return res.redirect('back');
+            });
+        }else{
+            console.log('You cannot delete this post');
+            return res.redirect('back');
+        }
     });
 }
