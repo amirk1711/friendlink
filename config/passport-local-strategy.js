64f9a9c84fb64d0daa04381d66ecaa2de1d92e63
Amir Khan <ak9471714@gmail.com>
2021-05-28 18:57:29 +0530

    Setup Noty for animating flash messages and set flash messages

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -12,12 +12,12 @@ passport.use(new LocalStrategy({
     function(req, email, password, done){
         User.findOne({email: email}, function(err, user){
             if(err){
-                console.log('Error in finding user --> passport', err);
+                req.flash('error', err);
                 return done(err);
             }
 
             if(!user || user.password != password){
-                console.log('Invalid username/password');
+                req.flash('error', 'Invalid username/password');
                 return done(null, false);
             }
 
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -8,10 +8,12 @@ module.exports.create = async function(req, res){
             user: req.user._id
         });
 
+        req.flash('success', 'Post published!');
+
         return res.redirect('back');
     } catch (err) {
-        console.log(`Error: ${err}`);
-        return;
+        req.flash('error', err);
+        return res.redirect('back');
     }
 };
 
@@ -25,13 +27,14 @@ module.exports.destroy = async function(req, res){
 
             await Comment.deleteMany({post: req.params.id});
             
+            req.flash('success', 'Post and associated comments deleted');
             return res.redirect('back');
         }else{
-            console.log('You cannot delete this post');
+            req.flash('error', 'You cannot delete this post');
             return res.redirect('back');
         }
     } catch (err) {
-        console.log(`Error: ${err}`);
-        return;
+        req.flash('error', err);
+        return res.redirect('back');
     }
 };
