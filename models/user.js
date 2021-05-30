7e3119c2a78646399da82e2d7a3dd558ff531158
Amir Khan <ak9471714@gmail.com>
2021-05-30 14:43:34 +0530

    Setup and configure multer fro file uploads

--- models/user.js
+++ models/user.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 const { schema } = require('../../codeial/models/user');
+const multer = require('multer');
+const path = require('path');
+
+// this is where i'll be storing all the avatars
+const AVATAR_PATH = path.join('/uploads/users/avatar');
+
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -14,10 +20,31 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
+    },
+    avatar: {
+        type: String
     }
 }, {
     timestamps: true
 });
+
+// now link all three, avatar(which is in schema),
+// AVATAR_PATH and multer to mak esure that
+let storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, path.join(__dirname, "..", AVATAR_PATH));
+	},
+	// if multiple users upload files with same name
+	filename: function (req, file, cb) {
+		cb(null, file.fieldname + "-" + Date.now());
+	},
+});
+
+// static functions
+// it is for whole user schema not for individual user
+userSchema.statics.uploadedAvatar = multer({ storage: storage }).single('avatar');
+// to make AVATAR_PATH publicly available to user schema
+userSchema.statics.avatarPath = AVATAR_PATH;
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
