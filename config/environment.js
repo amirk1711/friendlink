519399c342f0a26cc6dde83be176255b865876b3
Amir Khan <ak9471714@gmail.com>
2021-06-03 00:16:18 +0530

    Create environment variables for production mode

--- config/environment.js
+++ config/environment.js
@@ -20,7 +20,24 @@ const development = {
 }
 
 const production = {
-    name: 'production'
+    name: process.env.FRIENDLINK_ENVIRONMENT,
+    asset_path: process.env.FRIENDLINK_ASSET_PATH,
+    session_cookie_key: process.env.FRIENDLINK_SESSION_COOKIE_KEY,
+    db: process.env.FRIENDLINK_DB,
+    smtp: {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.FRIENDLINK_GMAIL_USERNAME,
+            pass: process.env.FRIENDLINK_GMAIL_PASSWORD,
+        }
+    },
+    google_client_id: process.env.FRIENDLINK_GOOGLE_CLIENT_ID,
+    google_client_secret: process.env.FRIENDLINK_GOOGLE_CLIENT_SECRET,
+    google_callback_url: process.env.FRIENDLINK_GOOGLE_CALLBACK_URL,
+    jwt_secret: process.env.FRIENDLINK_JWT_SECRET,
 }
 
-module.exports = development;
+module.exports = eval(process.env.FRIENDLINK_ENVIRONMENT) == undefined ? development : eval(process.env.FRIENDLINK_ENVIRONMENT); 
