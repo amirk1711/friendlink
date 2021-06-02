3cee032057cccc560405ef5782ffaf24a58a0c91
Amir Khan <ak9471714@gmail.com>
2021-06-02 11:03:59 +0530

    Setup router and controller dir for likes

---
+++ controllers/likes_controller.js
@@ -0,0 +1,7 @@
+const Like = require('../models/likes');
+const Post = require('../models/post');
+const Comment = require('../models/comment');
+
+module.exports.toggleLike = function(req, res){
+    
+}
