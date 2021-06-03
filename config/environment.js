8f61aadc02b6967d198854fe303b89de53a2bf44
Amir Khan <ak9471714@gmail.com>
2021-06-03 07:01:22 +0530

    1.Install Gulp and sub-middlewares of gulp 2.Configure Gulp to minify css and js

--- config/environment.js
+++ config/environment.js
@@ -14,7 +14,7 @@ const accessLogStream = rfs.createStream('access.log', {
 
 const development = {
     name: 'development',
-    asset_path: 'assets',
+    asset_path: '/assets',
     session_cookie_key: 'blahsomething',
     db: 'friendlink_development',
     smtp: {
