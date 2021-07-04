b00f92f6c168db1e5fce205bbf0bafcf439128ab
Amir Khan <ak9471714@gmail.com>
2021-07-04 12:41:20 +0530

    Add Create Post Api

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -11,13 +11,44 @@ module.exports.home = async function (req, res) {
 			},
 		})
 		.populate("likes");
-		
+
 	return res.json(200, {
 		message: "List of posts",
 		posts: posts,
 	});
 };
-
+module.exports.create = async function (req, res){
+	module.exports.create = async function (req, res) {
+		try {			
+			// create post
+			let post = await Post.create({
+				user: req.user.id,
+				content: content,
+				contentType: contentType,
+			})
+	
+			if (req.xhr) {
+				console.log("Create Post using AJAX");
+				// to populate just the name of the user (we'll not want to send the password in the API)
+				post = await post.populate("user", "name").execPopulate();
+				console.log("post to ajax", post);
+				return res.status(200).json({
+					data: {
+						post: post,
+					},
+					message: "Post created using AJAX!",
+				});
+			}
+		
+			req.flash("success", "Post published!");
+			return res.redirect("back");
+	
+		} catch (error) {
+			req.flash("error", "Error in creating Post");
+			return res.redirect("back");
+		}
+	};
+}
 module.exports.destroy = async function (req, res) {
 	try {
 		let post = await Post.findById(req.params.id);
