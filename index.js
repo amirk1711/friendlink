9276c53e213ceb9194e9d2141acd393f5c3c5d27
Amir Khan <ak9471714@gmail.com>
2021-07-09 10:32:55 +0530

    Fix a typo

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-const express = require('express');
-const env = require('./config/environment');
-const dotenv = require('dotenv');
-const helmet = require('helmet');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const env = require("./config/environment");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
 const app = express();
-require('./config/view-helper')(app);
+require("./config/view-helper")(app);
 const port = process.env.PORT || 8000;
-const expressLayouts = require('express-ejs-layouts');
-const db = require('./config/mongoose');
+const expressLayouts = require("express-ejs-layouts");
+const db = require("./config/mongoose");
 
-const session = require('express-session');
-const passport = require('passport');
-const passportLocal = require('./config/passport-local-strategy');
-const passportJWT = require('./config/passport-jwt-strategy');
-const passportGoogle = require('./config/passport-google-oauth2-strategy');
-const { pass } = require('./config/mongoose');
-const MongoStore = require('connect-mongo');
-const sassMiddleware = require('node-sass-middleware');
-const flash = require('connect-flash');
-const customMware = require('./config/middleware');
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
+const passportJWT = require("./config/passport-jwt-strategy");
+const passportGoogle = require("./config/passport-google-oauth2-strategy");
+const { pass } = require("./config/mongoose");
+const MongoStore = require("connect-mongo");
+const sassMiddleware = require("node-sass-middleware");
+const flash = require("connect-flash");
+const customMware = require("./config/middleware");
 
-const path = require('path');
-const { Server } = require('http');
+const path = require("path");
+const { Server } = require("http");
 
 // to use dotenv
 dotenv.config();
-
-
 
 const uri = process.env.MONGODB_URI;
 
@@ -35,20 +33,21 @@ const uri = process.env.MONGODB_URI;
 // src: from where to pick scss file
 // dest: where css file will be placed after compiling scss
 // Note: you must place sass-middleware before `express.static`
-// prefix - (String) It will tell the sass middleware that 
+// prefix - (String) It will tell the sass middleware that
 // any request file will always be prefixed with <prefix> and this prefix should be ignored.
 
 // run sass middleware only in development mode
-if(env.name == 'development'){
-    app.use(sassMiddleware({
-        src: path.join(__dirname, env.asset_path, 'scss'),
-        dest: path.join(__dirname, env.asset_path, 'css'),
-        debug: false,
-        outputStyle: 'extended',
-        prefix: '/css' // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
-    }));
+if (env.name == "development") {
+	app.use(
+		sassMiddleware({
+			src: path.join(__dirname, env.asset_path, "scss"),
+			dest: path.join(__dirname, env.asset_path, "css"),
+			debug: false,
+			outputStyle: "extended",
+			prefix: "/css", // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+		})
+	);
 }
-
 
 // express.urlencoded will extract the data from the form and add them into req.body
 app.use(express.urlencoded({ extended: true }));
@@ -56,51 +55,53 @@ app.use(cookieParser());
 app.use(express.static(env.asset_path));
 
 // make the uploads path available to the browser
-app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
 // app.use(express.json());
 // use helmet
-app.use(helemt());
+app.use(helmet());
 //to create log for console statments
-app.use(logger(env.morgan.mode , env.morgan.options));
+app.use(logger(env.morgan.mode, env.morgan.options));
 
 // tell the app to use express layouts
 app.use(expressLayouts);
 
 // extract the styles and scripts from sub pages into the layouts
-app.set('layout extractStyles', true);
-app.set('layout extractScripts', true);
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 // setup the view engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 // add a middleware which encrypt the session cookie
-app.use(session({
-    name: 'friendlink',
-    // change the secret before deployment in producction mode
-    secret: env.session_cookie_key,
-    saveUninitialized: false,
-    resave: false,
-    cookie: {
-        maxAge: (1000 * 60 * 1000)
-    },
-    // mongo store is used to store session cookie in the db
-    store: MongoStore.create(
-        {
-            // mongooseConnection: db,
-            mongoUrl: uri || 'mongodb://localhost/friendlink-development',
-            autoRemove: 'disabled'
-        },
-        function(err){
-            if(err){
-                console.log(`Error in mongo store: ${err}`);
-            }else{
-                console.log('connect-mongo setup ok');
-            }
-        }
-    )
-}));
+app.use(
+	session({
+		name: "friendlink",
+		// change the secret before deployment in producction mode
+		secret: env.session_cookie_key,
+		saveUninitialized: false,
+		resave: false,
+		cookie: {
+			maxAge: 1000 * 60 * 1000,
+		},
+		// mongo store is used to store session cookie in the db
+		store: MongoStore.create(
+			{
+				// mongooseConnection: db,
+				mongoUrl: uri || "mongodb://localhost/friendlink-development",
+				autoRemove: "disabled",
+			},
+			function (err) {
+				if (err) {
+					console.log(`Error in mongo store: ${err}`);
+				} else {
+					console.log("connect-mongo setup ok");
+				}
+			}
+		),
+	})
+);
 
 // we need to tell the app to use passport
 app.use(passport.initialize());
@@ -109,7 +110,7 @@ app.use(passport.session());
 // set authenticated user in locals
 app.use(passport.setAuthenticatedUser);
 
-// all requests will have a req.flash() function 
+// all requests will have a req.flash() function
 // that can be used for flash messages.
 app.use(flash());
 // use this middleware to put flash into res.locals
@@ -118,13 +119,13 @@ app.use(customMware.setFlash);
 
 // handle the routes
 // writing index is not neccessary
-app.use('/', require('./routes/index'));
+app.use("/", require("./routes/index"));
 
 // make server listen
-app.listen(port, function(err){
-    if(err){
-        console.log(`Error in running server: ${err}`);
-        return;
-    }
-    console.log(`Server is running on port: ${port}`);
+app.listen(port, function (err) {
+	if (err) {
+		console.log(`Error in running server: ${err}`);
+		return;
+	}
+	console.log(`Server is running on port: ${port}`);
 });
