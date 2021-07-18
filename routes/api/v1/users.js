d698df92e1b9a3a6829ad4979f108f820ca2a9b4
Amir Khan <ak9471714@gmail.com>
2021-07-18 09:42:56 +0530

    Change authentication to jwt for APIs

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -5,11 +5,11 @@ const usersApi = require("../../../controllers/api/v1/users_api");
 const passport = require("passport");
 
 router.post("/signup", usersApi.create);
-router.post("/login", passport.checkAuthentication, usersApi.createSession);
-router.get("/profile/:id", passport.checkAuthentication, usersApi.profile);
-router.post("/update/:id", passport.checkAuthentication, usersApi.update);
-router.delete("/:id", passport.checkAuthentication, usersApi.delete);
-router.put("/follow/:id", passport.checkAuthentication, usersApi.follow);
-router.put("/unfollow/:id", passport.checkAuthentication, usersApi.unfollow);
+router.post("/login", passport.authenticate("jwt", { session: false }), usersApi.createSession);
+router.get("/profile/:id", passport.authenticate("jwt", { session: false }), usersApi.profile);
+router.post("/update/:id", passport.authenticate("jwt", { session: false }), usersApi.update);
+router.delete("/:id", passport.authenticate("jwt", { session: false }), usersApi.delete);
+router.put("/follow/:id", passport.authenticate("jwt", { session: false }), usersApi.follow);
+router.put("/unfollow/:id", passport.authenticate("jwt", { session: false }), usersApi.unfollow);
 
 module.exports = router;
