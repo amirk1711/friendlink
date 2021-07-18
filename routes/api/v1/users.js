9854c2f920b3930b39e897b8f422c675a0f98f9d
Amir Khan <ak9471714@gmail.com>
2021-07-18 09:59:26 +0530

    Remove auth check from login route

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -5,7 +5,7 @@ const usersApi = require("../../../controllers/api/v1/users_api");
 const passport = require("passport");
 
 router.post("/signup", usersApi.create);
-router.post("/login", passport.authenticate("jwt", { session: false }), usersApi.createSession);
+router.post("/login", usersApi.createSession);
 router.get("/profile/:id", passport.authenticate("jwt", { session: false }), usersApi.profile);
 router.post("/update/:id", passport.authenticate("jwt", { session: false }), usersApi.update);
 router.delete("/:id", passport.authenticate("jwt", { session: false }), usersApi.delete);
