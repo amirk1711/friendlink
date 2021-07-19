9213ca47e2c1c14b9fff776a38e99df75d9bd38a
Amir Khan <ak9471714@gmail.com>
2021-07-19 15:40:41 +0530

    Add jwt auth in comments api route

--- routes/api/v1/comments.js
+++ routes/api/v1/comments.js
@@ -4,7 +4,7 @@ const passport = require("passport");
 
 const commentsApi = require("../../../controllers/api/v1/comments_api");
 
-router.post("/create", passport.checkAuthentication, commentsApi.create);
+router.post("/create", passport.authenticate("jwt", { session: false }), commentsApi.create);
 router.get("/destroy/:id", passport.checkAuthentication, commentsApi.destroy);
 
 module.exports = router;
