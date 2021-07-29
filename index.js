f7419f49e663172eb48f2a61bdc099ca1f200fd6
Amir Khan <ak9471714@gmail.com>
2021-07-30 00:39:45 +0530

    Add CORS headers

--- index.js
+++ index.js
@@ -38,6 +38,12 @@ app.use(
 	})
 );
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // to use dotenv
 dotenv.config();
 
