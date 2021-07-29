f2e70c28fbe6bae3670b823aac7aa47c4a6c9650
Amir Khan <ak9471714@gmail.com>
2021-07-30 00:58:07 +0530

    commit

--- controllers/api/v1/chats_api.js
+++ controllers/api/v1/chats_api.js
@@ -3,11 +3,11 @@ const Chat = require("../../../models/chat");
 module.exports.home = async function (req, res) {
 	try {
 		const chat = new Chat(req.body);
-
-		await chat.save();
-
+		
 		chat = await chat.populate("sender", "-password").populate("chatUserId").execPopulate();
 		console.log('chat', chat);
+
+		await chat.save();
 
 		return res.status(200).json({
 			message: "Chat created successfully",
@@ -17,7 +17,7 @@ module.exports.home = async function (req, res) {
 			},
 		});
 	} catch (error) {
-		return res.stateus(500).json(error);
+		return res.status(500).json(error);
 	}
 };
 
